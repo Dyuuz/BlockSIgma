6bbc337e2db1f2efb9533b7cbc2c734251f90774
Dyuuz <lekanoyesunle@gmail.com>
2025-09-06 21:45:32 +0100

    feat: update log on 30secs cron job interval

--- app/views/predictions/prediction_main_4hr.py
+++ app/views/predictions/prediction_main_4hr.py
@@ -593,7 +593,7 @@ async def get_current_predictions() -> None:
 
         del tickers, all_prices, all_assets
         gc.collect()
-        log_memory("4hr Current memory usage after refresh")
+        log_memory("4hr Current memory usage after successive update")
 
     except Exception as e: 
         print(f"Error during 4hr prediction data update: {e}")
